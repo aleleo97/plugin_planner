1d911a5ac2fad474577b62c259e47372518a2389
RDaneelOlivaw <duckfrost@gmail.com>
2020-07-07 12:45:42 +0000

    work1

--- src/my_global_planner.cpp
+++ src/my_global_planner.cpp
@@ -9,106 +9,79 @@
  //register this planner as a BaseGlobalPlanner plugin
  PLUGINLIB_EXPORT_CLASS(my_global_planner::MyGlobalPlanner, nav_core::BaseGlobalPlanner)
  
+ nav_msgs::Path my_plan;
  bool gProxyFlag ;
-
    void pathCallback(const nav_msgs::Path& msg){
-       
        ROS_INFO("proxy called");
-       printf("proxy called");
        gProxyFlag = true;
-
+       my_plan = msg;
+   }
+ 
+ void subProxy(){
+       ros::Rate Rate(10);
+       ros::NodeHandle nh;
+       ros::Subscriber sub = nh.subscribe("path_proxy", 1, pathCallback);
+       while(gProxyFlag == false){
+         ROS_INFO("Waiting a valid plan");
+         boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
+     }
    }
 
  namespace my_global_planner {
- 
    MyGlobalPlanner::MyGlobalPlanner()
    : costmap_ros_(NULL), initialized_(false){}
- 
    MyGlobalPlanner::MyGlobalPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
    : costmap_ros_(NULL), initialized_(false){
+     int argc = 0;
+     char** argv = NULL;
+     ros::init(argc,argv,name);
      initialize(name, costmap_ros);
    }
-
    void MyGlobalPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros){
      if(!initialized_){
        printf("Planner inizialised");
        ROS_INFO("This planner has been initialized");
        costmap_ros_ = costmap_ros;
        costmap_ = costmap_ros_->getCostmap();
- 
        ros::NodeHandle private_nh("~/" + name);
        private_nh.param("step_size", step_size_, costmap_->getResolution());
        private_nh.param("min_dist_from_robot", min_dist_from_robot_, 0.10);
-       //world_model_ = new base_local_planner::CostmapModel(*costmap_); 
-
-       ros::Subscriber sub = private_nh.subscribe("path_proxy", 1000, pathCallback);
        initialized_ = true;
        gProxyFlag = false;
      }
      else
        ROS_WARN("This planner has already been initialized... doing nothing");
    }
- 
 
- 
- 
    bool MyGlobalPlanner::makePlan(const geometry_msgs::PoseStamped& start, 
        const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan){
- 
      if(!initialized_){
        ROS_ERROR("The planner has not been initialized, please call initialize() to use the planner");
        return false;
-     }
- 
+     } 
      ROS_DEBUG("Got a start: %.2f, %.2f, and a goal: %.2f, %.2f", start.pose.position.x, start.pose.position.y, goal.pose.position.x, goal.pose.position.y);
-     ROS_INFO(gProxyFlag ? "true" : "false");
-     //printf( gProxyFlag ? "true" : "false");//
      plan.clear();
      costmap_ = costmap_ros_->getCostmap();
- 
-     const double start_yaw = tf2::getYaw(start.pose.orientation);
-     const double goal_yaw = tf2::getYaw(goal.pose.orientation);
- 
-     //we want to step back along the vector created by the robot's position and the goal pose until we find a legal cell
-     double goal_x = goal.pose.position.x;
-     double goal_y = goal.pose.position.y;
-     double start_x = start.pose.position.x;
-     double start_y = start.pose.position.y;
- 
-     double diff_x = goal_x - start_x;
-     double diff_y = goal_y - start_y;
-     double diff_yaw = angles::normalize_angle(goal_yaw-start_yaw);
- 
-     double target_x = goal_x;
-     double target_y = goal_y;
-     double target_yaw = goal_yaw;
- 
-     bool done = false;
-     double scale = 1.0;
-     double dScale = 0.01;
-
-     while(gProxyFlag == false){
-         ROS_INFO("Waiting a valid plan");
-         ROS_INFO(gProxyFlag ? "true" : "false");
-     }
-     ROS_INFO(gProxyFlag ? "true" : "false");
-     target_yaw = angles::normalize_angle(start_yaw + scale * diff_yaw);
- 
+     ROS_INFO("path created");
+     boost::thread t1 (&subProxy);
+     t1.join();
      plan.push_back(start);
-     geometry_msgs::PoseStamped new_goal = goal;
-     tf2::Quaternion goal_quat;
-     goal_quat.setRPY(0, 0, target_yaw);
- 
-     new_goal.pose.position.x = target_x;
-     new_goal.pose.position.y = target_y;
- 
-     new_goal.pose.orientation.x = goal_quat.x();
-     new_goal.pose.orientation.y = goal_quat.y();
-     new_goal.pose.orientation.z = goal_quat.z();
-     new_goal.pose.orientation.w = goal_quat.w();
- 
-     plan.push_back(new_goal);
-     return (done);
-   }
- 
- };
+     for(int i=0; i < (my_plan.poses.size());i++){
+        geometry_msgs::PoseStamped new_goal = goal;
+
+        new_goal.pose.position.x = my_plan.poses[i].pose.position.x;
+        new_goal.pose.position.y = my_plan.poses[i].pose.position.y;
+
+        new_goal.pose.orientation.x = my_plan.poses[i].pose.orientation.x;
+        new_goal.pose.orientation.y = my_plan.poses[i].pose.orientation.y;
+        new_goal.pose.orientation.z = my_plan.poses[i].pose.orientation.z;
+        new_goal.pose.orientation.w = my_plan.poses[i].pose.orientation.w;
+
+        plan.push_back(new_goal);
+    }
+    
+    plan.push_back(goal);
+    gProxyFlag = false;
+    return true;
+    }
+   };
--- src/my_global_planner.h
+++ src/my_global_planner.h
@@ -22,10 +22,8 @@
        MyGlobalPlanner();
        MyGlobalPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
 
-       //void pathCallback(const nav_msgs::Path& msg);
 
        void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
- 
        bool makePlan(const geometry_msgs::PoseStamped& start, 
            const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan);
  
@@ -34,9 +32,7 @@
        double step_size_, min_dist_from_robot_;
        costmap_2d::Costmap2D* costmap_;
        base_local_planner::WorldModel* world_model_; 
-
        double footprintCost(double x_i, double y_i, double theta_i);
- 
        bool initialized_;
    };
  };  
